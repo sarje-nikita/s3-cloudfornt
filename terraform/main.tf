4a8bd90f2353491c0375f393321bd9d0824042fb
sarje-nikita <contact.nikitasarje@gmail.com>
2025-08-31 15:18:03 +0530

    inital commit

---
+++ terraform/main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  } 
+
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "website" {
+  bucket = "cicd-demo-bucket-12345"
+  website {
+    index_document = "index.html"
+  }
+} 
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.website.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.website.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.website.arn}/*"
+    }]
+  })
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = "s3-website"
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-website"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+} 
